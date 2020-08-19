f61381a53aa22c5ceb255d662c6440d164b1a981
Khurshid Alam Jony <khurshidalam741@gmail.com>
2020-08-19 23:48:05 +0600

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        {{name}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: 'This is component Example'
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
